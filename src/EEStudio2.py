6f6fcee537c2ab59b7a445618777f59fd7ae20c6
zocker-160 <zocker1600@posteo.net>
2020-12-24 17:11:04 +0100

    update CI CD for submodule update

--- src/EEStudio2.py
+++ src/EEStudio2.py
@@ -25,7 +25,7 @@ from lib.SSAtool.src import SSAtool
 from lib.SSTtool.src import SSTtool
 from lib.SSTslicer.src import SSTslicer
 
-VERSION = "v0.2.1"
+VERSION = "v0.2.2"
 PLACEHOLDER_STR = "$$$"
 
 class AboutWindow(QDialog, Ui_aboutWindow.Ui_Dialog):
