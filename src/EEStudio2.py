8cd0cffa8ad23b7357e5f9f675bab6bd2ada5d38
zocker-160 <zocker1600@posteo.net>
2021-03-29 13:03:57 +0200

    bump version to 0.2.5

--- src/EEStudio2.py
+++ src/EEStudio2.py
@@ -25,7 +25,7 @@ from lib.SSAtool.src import SSAtool
 from lib.SSTtool.src import SSTtool
 from lib.SSTslicer.src import SSTslicer
 
-VERSION = "v0.2.4"
+VERSION = "v0.2.5"
 PLACEHOLDER_STR = "$$$"
 
 class AboutWindow(QDialog, Ui_aboutWindow.Ui_Dialog):
