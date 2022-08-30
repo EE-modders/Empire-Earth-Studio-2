9a057c8112fba6ba1227c1445bb0cf79d80ad90e
zocker-160 <zocker1600@posteo.net>
2022-08-30 20:00:31 +0200

    regenerate and move ui files

--- src/ui/mainWindowAssets_rc.py
+++ src/ui/mainWindowAssets_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.12.8)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.3)
 #
 # WARNING! All changes made in this file will be lost!
 
