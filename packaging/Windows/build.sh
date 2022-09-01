bc3b79b61ffa0dbdf6155b2eacdd76c39c6e34fd
zocker-160 <zocker1600@posteo.net>
2022-09-01 11:27:50 -0700

    add package script for Windows

---
+++ packaging/Windows/build.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+function cleanup {
+	echo "cleaning up"
+	rm -rf ~/Downloads/EES2 || true
+}
+
+cleanup
+rm -rf ~/Downloads/EEStudio2 || true
+
+echo "copying files"
+cp -r ~/Documents/Empire-Earth-Studio-2/src ~/Downloads/EES2
+
+cd ~/Downloads/EES2/
+cp assets/EESicon.ico .
+
+echo "running pyinstaller"
+pyinstaller --icon=EESicon.ico --noconsole EEStudio2.py
+
+echo "copy files post installation"
+cp -r ./assets dist/EEStudio2
+mkdir -p dist/EEStudio2/lib/SSA
+cp lib/SSA/libblast.dll dist/EEStudio2/lib/SSA
+
+cp -r dist/EEStudio2 ~/Downloads
+
+cleanup
